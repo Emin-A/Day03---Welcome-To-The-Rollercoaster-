051bf51af5fdace656839fcc8152b6ac77e0f438
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-11 15:05:55 +0000

    Explanation

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# CONTROL FLOWS, CONDITIONALS, COMPARISON OPERATORS
+# Comparison Operators : >,<,>=,<=,==,!=, = -Assigning, == -Checking
+
 # Welcome to the rollercoaster ! 
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
@@ -6,3 +9,4 @@ if height >= 120:
   print("You can ride the rollercoaster!")
 else:
   print("Sorry, you have to grow a little taller before you can ride!")
+
